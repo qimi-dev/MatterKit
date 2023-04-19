8fa0841a109c7a05b1fbab742c7ee2252fe43214
qimi <15217710611@163.com>
2023-04-20 02:08:07 +0800

    Update the status bar style

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.accompanist.systemuicontroller)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
 }
--- app/src/main/java/com/qimidev/app/matterkit/MainActivity.kt
+++ app/src/main/java/com/qimidev/app/matterkit/MainActivity.kt
@@ -3,9 +3,14 @@ package com.qimidev.app.matterkit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.google.accompanist.systemuicontroller.SystemUiController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.qimidev.app.matterkit.ui.MatterKitApp
 import com.qimidev.app.matterkit.core.ui.theme.MatterKitTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,17 +18,22 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private lateinit var windowInsetsController: WindowInsetsControllerCompat
+    private var systemUiController: SystemUiController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
-        windowInsetsController.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-
         setContent {
             MatterKitTheme {
+                systemUiController = rememberSystemUiController()
+                val primaryColor: Color = MaterialTheme.colorScheme.background
+                SideEffect {
+                    systemUiController!!.isNavigationBarVisible = false
+                    systemUiController!!.systemBarsBehavior =
+                        WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                    systemUiController!!.setStatusBarColor(primaryColor)
+                }
+
                 MatterKitApp()
             }
         }
@@ -31,7 +41,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
+        systemUiController?.isNavigationBarVisible = false
     }
 
 }
